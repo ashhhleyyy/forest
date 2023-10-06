5d825d97cef45abd4af8863a2e614586f7d19ce5
Ashhhleyyy <ash@ashhhleyyy.dev>
2023-10-06 16:26:45 +0100

    feat: add git configs

--- home/ash/desktop.nix
+++ home/ash/desktop.nix
@@ -15,4 +15,20 @@
     theme = "Catppuccin-Mocha";
     font.name = "JetBrainsMono Nerd Font";
   };
+
+  programs.git = {
+    enable = true;
+    userName = "Ashhhleyyy";
+    userEmail = "ash@ashhhleyyy.dev";
+    signing = {
+      key = "83B789081A0878FB";
+      signByDefault = true;
+    };
+  };
+
+  programs.gpg = {
+    enable = true;
+    mutableKeys = true;
+    mutableTrust = rue;
+  };
 }
