ad227aaf1a0275a0f8f091f68a43422c2b2386cf
Ashhhleyyy <ash@ashhhleyyy.dev>
2023-12-27 13:22:15 +0000

    feat: add kdeconnect

--- home/ash/alex.nix
+++ home/ash/alex.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, ... }: {
+{ config, nixpkgs, pkgs, ... }: {
   home.packages = with pkgs; [
     jetbrains.gateway
     obsidian
   ];
+  services.kdeconnect = {
+    enable = true;
+    indicator = true;
+  };
 }
