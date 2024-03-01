1a0fe985f13e7d380ae7614f3e574a4b07154b42
Ashhhleyyy <ash@ashhhleyyy.dev>
2024-03-01 16:27:58 +0000

    feat(alex): add korganizer

--- home/ash/alex.nix
+++ home/ash/alex.nix
@@ -4,6 +4,17 @@
     obsidian
     libresprite
     plover.dev
+    libsForQt5.korganizer
+    libsForQt5.kdepim-runtime
+    libsForQt5.kaddressbook
+    libsForQt5.akonadi-notes
+    libsForQt5.akonadi-mime
+    libsForQt5.akonadi-search
+    libsForQt5.akonadi-contacts
+    libsForQt5.akonadi-calendar
+    libsForQt5.akonadi-import-wizard
+    libsForQt5.akonadiconsole
+    libsForQt5.akonadi
   ];
   services.kdeconnect = {
     enable = true;
