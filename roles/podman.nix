d317fa22c9b3d96871cc07d5248ce072b72de87f
Ashhhleyyy <ash@ashhhleyyy.dev>
2024-09-23 16:20:59 +0100

    feat: enable podman docker compat

--- roles/podman.nix
+++ roles/podman.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }: {
-  virtualisation.podman.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    dockerSocket.enable = true;
+  };
   virtualisation.oci-containers.backend = "podman";
   environment.systemPackages = with pkgs; [
     podman-compose
