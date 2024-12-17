a37e129495e80ee216541a4c27731442dccd18f2
Ashhhleyyy <ash@ashhhleyyy.dev>
2024-12-17 12:11:57 +0000

    feat(conduit): add matrix web clients

--- roles/conduit.nix
+++ roles/conduit.nix
@@ -11,11 +11,31 @@
     };
   };
 
-  services.caddy.virtualHosts."matrix.shorks.gay".extraConfig = ''
-    reverse_proxy 127.0.0.1:6167
+  services.caddy.virtualHosts = {
+    "matrix.shorks.gay".extraConfig = ''
+      reverse_proxy 127.0.0.1:6167
 
-    log {
-      output file /var/log/caddy/matrix.shorks.gay-access.log
-    }
-  '';
+      log {
+        output file /var/log/caddy/matrix.shorks.gay-access.log
+      }
+    '';
+
+    "schildi.shorks.gay".extraConfig = ''
+      root * /var/www/schildichat-web
+      file_server
+
+      log {
+        output file /var/log/caddy/schildi.shorks.gay-access.log
+      }
+    '';
+
+    "cinny.shorks.gay".extraConfig = ''
+      root * /var/www/cinny
+      file_server
+
+      log {
+        output file /var/log/caddy/cinny.shorks.gay-access.log
+      }
+    '';
+  };
 }
