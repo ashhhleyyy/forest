a0f15d0d44b2945910356417f899842c012c410d
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-01-01 01:42:38 +0000

    fix(jenkins): put bash on path

--- roles/jenkins.nix
+++ roles/jenkins.nix
@@ -1,7 +1,9 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   services.jenkins = {
     enable = true;
     listenAddress = "localhost";
+
+    packages = [ pkgs.stdenv pkgs.git pkgs.jdk17 pkgs.bash config.programs.ssh.package pkgs.nix ];
   };
 
   services.caddy.virtualHosts."jenkins.service.isnt-a.top".extraConfig = ''
