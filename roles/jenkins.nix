1686a43a52a22032be9a4485d8651f06807a4ec9
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-04-07 18:13:27 +0100

    feat: remove caddy configs for kubernetesified(tm) services

--- roles/jenkins.nix
+++ roles/jenkins.nix
@@ -5,8 +5,4 @@
 
     packages = [ pkgs.stdenv pkgs.git pkgs.jdk17 pkgs.bash config.programs.ssh.package pkgs.nix ];
   };
-
-  services.caddy.virtualHosts."jenkins.service.isnt-a.top".extraConfig = ''
-    reverse_proxy localhost:8080
-  '';
 }
--- roles/uptime-kuma.nix
+++ roles/uptime-kuma.nix
@@ -6,8 +6,4 @@
       PORT = "4000";
     };
   };
-
-  services.caddy.virtualHosts."monitoring.service.ashhhleyyy.dev".extraConfig = ''
-    reverse_proxy localhost:4000
-  '';
 }
