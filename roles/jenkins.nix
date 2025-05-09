8f7d1d48c1334ef155c623ea9824e357e83d45d6
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-05-09 12:24:40 +0100

    fix(jenkins): enable podman docker compat

--- roles/jenkins.nix
+++ roles/jenkins.nix
@@ -5,4 +5,6 @@
 
     packages = [ pkgs.stdenv pkgs.git pkgs.jdk17 pkgs.bash config.programs.ssh.package pkgs.nix ];
   };
+
+  virtualisation.podman.dockerCompat = config.virtualisation.podman.enable;
 }
