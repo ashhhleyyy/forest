455c287559352c5ce37635424e8d775ef94a70f2
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-07-22 13:09:44 +0100

    feat(soju): add ircs listener

--- roles/soju.nix
+++ roles/soju.nix
@@ -1,15 +1,26 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }:
+
+let tls-dir = config.security.acme.certs."${config.networking.hostName}.net.isnt-a.top".directory;
+
+{
   services.soju = {
     enable = true;
     # listen only over tailscale
-    listen = ["irc+insecure://100.97.123.128"];
+    listen = [
+      "irc+insecure://100.97.123.128"
+      "ircs://0.0.0.0"
+    ];
     adminSocket.enable = true;
     # we store in the db
     enableMessageLogging = false;
     extraConfig = ''
       message-store db
     '';
+    tlsCertificate = "${tls-dir}/fullchain.pem";
+    tlsCertificateKey = "${tls-dir}/key.pem";
   };
+
+  networking.firewall.allowedTCPPorts = [ 6697 ];
 
   environment.systemPackages = with pkgs; [
     soju
