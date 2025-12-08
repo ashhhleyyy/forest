fba26efefd13acdb6fb266e0cee0519ed9df0cb4
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-12-08 13:09:51 +0000

    fix(immich): bind to different port

--- roles/immich.nix
+++ roles/immich.nix
@@ -7,6 +7,9 @@
     enable = true;
     database.enableVectors = false;
     host = "::";
+    machine-learning.environment = {
+      IMMICH_PORT = "3007";
+    };
   };
 
   services.postgresqlBackup.databases = ["immich"];
