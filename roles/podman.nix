c6a97f7ea9191db1ad9e5d1f2924da89cad375f8
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-12-22 14:03:30 +0000

    fix(podman): mark jessica registry as insecure

--- roles/podman.nix
+++ roles/podman.nix
@@ -3,6 +3,9 @@
     enable = true;
     dockerSocket.enable = true;
   };
+  virtualisation.containers.registries.insecure = [
+    "jessica:5000"
+  ];
   virtualisation.oci-containers.backend = "podman";
   environment.systemPackages = with pkgs; [
     podman-compose
