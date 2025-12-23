102865a644836116634dddd1350e056c1fe7f36f
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-12-23 00:36:34 +0000

    feat(garage): add full settings

--- roles/garage.nix
+++ roles/garage.nix
@@ -4,7 +4,24 @@
   services.garage = {
     enable = true;
     package = pkgs.garage_2;
-    settings = {};
+    settings = {
+      replication_factor = 1;
+      rpc_bind_addr = "[::]:3901";
+      rpc_public_addr = "127.0.0.1:3901";
+      s3_api = {
+        s3_region = "garage";
+        api_bind_addr = "[::]:3900";
+        root_domain = "s3-garage.service.isnt-a.top";
+      };
+      s3_web = {
+        bind_addr = "[::]:3902";
+        root_domain = "web-garage.service.isnt-a.top";
+        index = "index.html";
+      };
+      admin = {
+        api_bind_addr = "[::]:3903";
+      };
+    };
   };
 
   forest.backups.paths = [ "/var/lib/garage" ];
