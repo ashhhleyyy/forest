2ea74aef5424893f21a2b64bd7d27dbdf2173de3
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-02-24 13:09:36 +0000

    chore: remove some jetbrains IDEs i can easily replace with intellij+plugins

--- home/ash/intellij.nix
+++ home/ash/intellij.nix
@@ -1,10 +1,7 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [
     jetbrains.idea
-    jetbrains.phpstorm
     jetbrains.rider
-    jetbrains.pycharm
     android-studio
-    jetbrains-toolbox
   ];
 }
