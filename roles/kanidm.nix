d4bd8804579dfe4de5d82ebf2ed7397eb117b1b9
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-06-05 02:48:37 +0100

    chore(kanidm): change to new config names

--- roles/kanidm.nix
+++ roles/kanidm.nix
@@ -8,29 +8,32 @@ in
   services.kanidm = {
     package = pkgs.kanidm_1_10;
 
-    enableServer = true;
-    serverSettings = {
-      version = "2";
-      bindaddress = "[::]:3006";
-      ldapbindaddress = "[::]:3636";
-      tls_chain = "${tls-dir}/fullchain.pem";
-      tls_key = "${tls-dir}/key.pem";
-      log_level = "info";
-      domain = "sso.ashhhleyyy.dev";
-      origin = "https://sso.ashhhleyyy.dev";
-      role = "WriteReplica";
-      http_client_address_info."x-forward-for" = [];
-      online_backup = {
-        path = "/var/lib/kanidm/backups/";
-        schedule = "0 3 * * *";
-        versions = 7;
+    server = {
+      enable = true;
+      settings = {
+        version = "2";
+        bindaddress = "[::]:3006";
+        ldapbindaddress = "[::]:3636";
+        tls_chain = "${tls-dir}/fullchain.pem";
+        tls_key = "${tls-dir}/key.pem";
+        log_level = "info";
+        domain = "sso.ashhhleyyy.dev";
+        origin = "https://sso.ashhhleyyy.dev";
+        role = "WriteReplica";
+        http_client_address_info."x-forward-for" = [];
+        online_backup = {
+          path = "/var/lib/kanidm/backups/";
+          schedule = "0 3 * * *";
+          versions = 7;
+        };
       };
     };
 
-    enableClient = true;
-
-    clientSettings = {
-      uri = "https://sso.ashhhleyyy.dev";
+    client = {
+      enable = true;
+      settings = {
+        uri = "https://sso.ashhhleyyy.dev";
+      };
     };
   };
 
