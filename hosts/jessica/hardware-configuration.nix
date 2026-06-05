38f0e26f99695c3783eeaa5b161e63be452e8d85
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-06-05 02:27:10 +0100

    fix(jessica): add mount for /boot

--- hosts/jessica/hardware-configuration.nix
+++ hosts/jessica/hardware-configuration.nix
@@ -7,6 +7,7 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  fileSystems."/boot" = { device = "/dev/disk/by-uuid/C003-5D9C"; fsType = "vfat"; options = [ "fmask=0022" "dmask=0022" ]; };
   swapDevices = [ ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
