4ef7e206de1038a86b8484f80a8431ffc3006463
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-08-15 01:30:47 +0100

    fix(iocaine): include stuff from pkgs in corpus package properly

--- roles/iocaine/1984.nix
+++ roles/iocaine/1984.nix
@@ -1,9 +1,9 @@
-{ stdenvNoCC }:
+{ stdenvNoCC, fetchurl }:
 
 stdenvNoCC.mkDerivation {
   pname = "archive.org_1984.txt";
   version = "latest";
-  src = pkgs.fetchurl {
+  src = fetchurl {
     url = "https://archive.org/download/GeorgeOrwells1984/1984_djvu.txt";
     hash = "sha256-9R1PTa8yDtkfH+4rU5BF62ee73irhd3VYX1QB5KU+ZU=";
   };
