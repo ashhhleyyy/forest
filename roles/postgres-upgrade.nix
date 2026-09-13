840c1d39231e6ba07149b22989c992f894398eeb
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-09-14 00:43:41 +0100

    fix(postgres-upgrade): i should just kill this database

--- roles/postgres-upgrade.nix
+++ roles/postgres-upgrade.nix
@@ -11,6 +11,20 @@
       let
         newPostgres = pkgs.postgresql_18.withPackages (pp: [
           pp.pgroonga
+          (pkgs.stdenv.mkDerivation {
+            name = "zulip-dicts";
+            phases = "installPhase";
+            src = pkgs.fetchurl {
+              url = "https://raw.githubusercontent.com/zulip/zulip/dd678465aed915101f9a74054e28535bbdd88ba3/puppet/zulip/files/postgresql/zulip_english.stop";
+              hash = "sha256-F3CmCRkkPURN9Uo7KIFxkajSJsiTYQg1wubKCF2+bAs=";
+            };
+            installPhase = ''
+            mkdir -p $out/share/postgresql/tsearch_data/
+            ln -s ${pkgs.hunspellDicts.en_US}/share/hunspell/en_US.dic $out/share/postgresql/tsearch_data/en_us.dict
+            ln -s ${pkgs.hunspellDicts.en_US}/share/hunspell/en_US.aff $out/share/postgresql/tsearch_data/en_us.affix
+            cp $src $out/share/postgresql/tsearch_data/zulip_english.stop
+            '';
+          })
         ]);
         cfg = config.services.postgresql;
       in
