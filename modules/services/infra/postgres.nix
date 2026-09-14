3bc85c954a001fee8c9e9217ad9751251215daf6
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-09-14 21:14:26 +0100

    fix(postgres): listOf not listof

--- modules/services/infra/postgres.nix
+++ modules/services/infra/postgres.nix
@@ -23,7 +23,7 @@ in
       default = [];
     };
     users = lib.mkOption {
-      type = lib.types.listof lib.types.submodule {
+      type = lib.types.listOf lib.types.submodule {
         options = {
           name = lib.mkOption {
             type = lib.types.str;
