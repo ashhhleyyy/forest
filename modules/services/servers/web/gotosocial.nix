b11a78ff32e8e26b398077991223cc9ee722d430
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-09-14 01:13:41 +0100

    fix(gts): set trusted proxies correctly

--- modules/services/servers/web/gotosocial.nix
+++ modules/services/servers/web/gotosocial.nix
@@ -23,7 +23,7 @@ in
       settings = {
         host = cfg.host;
         port = 3001;
-        trusted-proxies = ["100.64.0.0/10"];
+        trusted-proxies = ["127.0.0.1/32"];
         bind-address = "0.0.0.0";
         accounts-registration-open = true;
         accounts-reason-required = true;
