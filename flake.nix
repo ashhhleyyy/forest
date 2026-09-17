6f2b3efcb2c58b99ce8ab6e5890f9c33ccb946ea
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-09-17 17:52:18 +0100

    chore: try reverting back to nixocaine upstream version of nsoe

--- flake.nix
+++ flake.nix
@@ -56,7 +56,6 @@
     nixocaine = {
       url = "https://git.madhouse-project.org/iocaine/nixocaine/archive/stable.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs-stable";
-      inputs.nam-shub-of-enki.url = "git+https://git.madhouse-project.org/iocaine/nam-shub-of-enki?ref=iocaine-3.x";
     };
 
     git-in = {
